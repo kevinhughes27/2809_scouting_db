eac1cf51343d89f52791b0e1330ddf5f0976a954
Sawyer <sawyer_sw@yahoo.ca>
2013-02-04 21:42:47 -0500

    Bug fixing
    -fixed a syntax error
    -added proper creation of new tables

--- scouting.py
+++ scouting.py
@@ -8,25 +8,27 @@ c = conn.cursor()
 while True:
     cmd = raw_input('Enter a Command: ')
 
-    elif cmd == 'input':
-        try:
-            team = int(raw_input('Team: '))
-            match = int(raw_input('match: '))
-            auto3 = int(raw_input('Autonomous Three Pointers: '))
-            auto2 = int(raw_input('Autonomous Two Pointers: '))
-            auto1 = int(raw_input('Autonomous One Pointers: '))
-            five = int(raw_input('Five Pointers: '))
-            three = int(raw_input('Three pointers: '))
-            two = int(raw_input('Two pointers: '))
-            one = int(raw_input('One Pointers: '))
-            climb = int(raw_input('climb score: '))
-            while climb != 10 and climb != 20 and climb != 30 and climb != 0:
-                print 'Invalid Input'
+    if cmd == 'input':
+        while num < 7:
+            
+            try:
+                team = int(raw_input('Team: '))
+                match = int(raw_input('match: '))
+                auto3 = int(raw_input('Autonomous Three Pointers: '))
+                auto2 = int(raw_input('Autonomous Two Pointers: '))
+                auto1 = int(raw_input('Autonomous One Pointers: '))
+                five = int(raw_input('Five Pointers: '))
+                three = int(raw_input('Three pointers: '))
+                two = int(raw_input('Two pointers: '))
+                one = int(raw_input('One Pointers: '))
                 climb = int(raw_input('climb score: '))
-            row = (team,match,auto1,auto2,auto3,five,three,two,one,climb)
-            c.execute('INSERT INTO teams VALUES (?,?,?,?,?,?,?,?,?,?)', row)
-        except ValueError:
-            print 'invalid number'
+                while climb != 10 and climb != 20 and climb != 30 and climb != 0:
+                    print 'Invalid Input, climb score can only be 0, 10, 20 or 30'
+                    climb = int(raw_input('climb score: '))
+                row = (team,match,auto1,auto2,auto3,five,three,two,one,climb)
+                c.execute('INSERT INTO teams VALUES (?,?,?,?,?,?,?,?,?,?)', row)
+            except ValueError:
+                print 'invalid number'
 
     elif cmd == 'output':
         for row in c.execute('SELECT * FROM teams ORDER BY team'):
@@ -38,12 +40,10 @@ while True:
         break
 
     elif cmd == 'clear':
-        c.execute('DROP TABLE teams')
+        c.execute('DROP TABLE if exists teams')
         c.execute('''CREATE TABLE teams
                     (Team, Match, AutoThree, AutoTwo, AutoOne,
                     FivePointers, ThreePointers, TwoPointers, OnePointers, ClimbScore)''')
         
     else:
         print 'unrecognized command'
-    
-
