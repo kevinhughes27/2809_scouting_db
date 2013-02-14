dff23cffa120dd3250975335a21d94a22e7e6ed9
Sawyer <sawyer_sw@yahoo.ca>
2013-02-13 21:39:11 -0500

    Changed Things
    +Added a compare teams program(compareTeams.py)
    +Added a Readme for mergeDb
    +Added a pick list generator (rankTeams.py)
    +fixed queryStat to query proper db stats
    +mergeDb now provides some feedback (just to prove it is doing
    something)

---
+++ mergeDb/README.txt
@@ -0,0 +1,8 @@
+mergeDb By Sawyer Shipp-Wiedersprecher
+for Kbotics 2013 scouting database
+
+
+1. Place both scouting.db files in 'file1' and 'file2' folders
+2. Run the program once
+3. The information in file2 will be injected into file1 
+4. Remove scouting.db from file1, you may delete file2 unless it is otherwise required
--- queryStat.py
+++ queryStat.py
@@ -4,35 +4,29 @@ c = conn.cursor()
 
 q = raw_input('Enter a stat to query: ')
 
-if q == 'AutoThree':
-    for row in c.execute('select team,avg(AutoThree) as avg_three from teams group by team'):
+if q == 'auto':
+    for row in c.execute('select team,avg(auto) as avg_auto from teams group by team order by avg_auto desc'):
         print row
 
-if q == 'AutoTwo':
-    for row in c.execute('select team,avg(AutoTwo) as avg_three from teams group by team'):
-        print row
-        
-if q == 'AutoOne':
-    for row in c.execute('select team,avg(AutoOne) as avg_three from teams group by team'):
-        print row
-
-if q == 'FivePointers':
-    for row in c.execute('select team,avg(FivePointers) as avg_three from teams group by team'):
+if q == 'five':
+    for row in c.execute('select team,avg(five) as avg_five from teams group by team order by avg_five desc'):
         print row
     
-if q == 'ThreePointers':
-    for row in c.execute('select team,avg(ThreePointers) as avg_three from teams group by team'):
+if q == 'three':
+    for row in c.execute('select team,avg(three) as avg_three from teams group by team order by avg_three desc'):
         print row
 
-if q == 'TwoPointers':
-    for row in c.execute('select team,avg(TwoPointers) as avg_three from teams group by team'):
+if q == 'two':
+    for row in c.execute('select team,avg(two) as avg_two from teams group by team order by avg_two desc'):
         print row
 
-if q == 'OnePointers':
-    for row in c.execute('select team,avg(OnePointers) as avg_three from teams group by team'):
+if q == 'one':
+    for row in c.execute('select team,avg(one) as avg_one from teams group by team order by avg_one desc'):
         print row
 
-if q == 'ClimbScore':
-    for row in c.execute('select team,avg(ClimbScore) as avg_three from teams group by team'):
+if q == 'climb':
+    for row in c.execute('select team,avg(climb) as avg_climb from teams group by team order by avg_climb desc'):
         print row
+
+raw_input('Press any key to continue...')
         
